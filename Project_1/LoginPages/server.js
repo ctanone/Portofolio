366fdd595f8a0c18c9f486e26e68e6c9f72f76e0
TUF-A15-A506NFR <ctanone@gmail.com>
2026-01-15 02:29:50 +0700

    Reorganize file

--- Project_1/LoginPages/server.js
+++ Project_1/LoginPages/server.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://c11200006:<nBwhRxYsbHVC9Cge>@testingapp.g611fh8.mongodb.net/?appName=Testingapp";
+const uri = "mongodb+srv://c11200006:<db_password>@testingapp.g611fh8.mongodb.net/?appName=Testingapp";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
